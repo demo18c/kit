4e581d505f333dc1ba43fbd1c518b754ec9bec90
RASHAD ABDULBARR <rashadabdulbarr@RASHADs-MacBook-Pro.local>
2021-02-20 07:55:18 -0500

    removed amplify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import react from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Amplify from 'aws-amplify';
-import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import awsconfig from './aws-exports';
+// import Amplify from 'aws-amplify';
+// import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+// import awsconfig from './aws-exports';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,34 +15,34 @@ import About from './components/About';
 import Three from './components/Three';
 import Two from './components/Two';
 
-Amplify.configure(awsconfig);
+// Amplify.configure(awsconfig);
 
 function App() {
 	return (
 		// <Home />
 		<>
-			<AmplifyAuthenticator>
-				<Header />
-				<Router>
-					<Switch>
-						<Route path="/Home">
-							<Home />
-						</Route>
-						<Route path="/IvasRadio">
-							<IvasRadio />
-						</Route>
-						<Route path="/About">
-							<About />
-						</Route>
-						<Route path="/Two">
-							<Two />
-						</Route>
-						<Route path="/Three">
-							<Three />
-						</Route>
-					</Switch>
-				</Router>
-			</AmplifyAuthenticator>
+			{/* <AmplifyAuthenticator> */}
+			<Header />
+			<Router>
+				<Switch>
+					<Route path="/Home">
+						<Home />
+					</Route>
+					<Route path="/IvasRadio">
+						<IvasRadio />
+					</Route>
+					<Route path="/About">
+						<About />
+					</Route>
+					<Route path="/Two">
+						<Two />
+					</Route>
+					<Route path="/Three">
+						<Three />
+					</Route>
+				</Switch>
+			</Router>
+			{/* </AmplifyAuthenticator> */}
 		</>
 	);
 }
